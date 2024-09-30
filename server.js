850e4ff7dba25f947e3ae232fee48e7043ef7134
Simon Alvarado <s.alvarado@viseven.com>
2024-09-29 23:03:33 -0300

    update(server): Updated server

--- server.js
+++ server.js
@@ -6,29 +6,22 @@ import cors from "cors";
 const app = express();
 const port = 6001;
 
-// Define la ruta del archivo de base de datos
 const file = "./db.json";
 
-// Define los datos por defecto
 const defaultData = { items: [] };
 
-// Crea un adaptador para el archivo
 const adapter = new JSONFile(file);
 
-// Inicializa Low con el adaptador y los datos por defecto
 const db = new Low(adapter, defaultData);
 
-// Middleware para parsear JSON
 app.use(express.json());
 app.use(cors());
 
-// Lee la base de datos antes de manejar cualquier solicitud
 app.use(async (req, res, next) => {
   await db.read();
   next();
 });
 
-// Ruta para obtener items con paginación y búsqueda
 app.get("/api/items", (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const perPage = parseInt(req.query.per_page) || 10;
@@ -36,14 +29,12 @@ app.get("/api/items", (req, res) => {
 
   let filteredItems = db.data.items;
 
-  // Aplicar filtro de búsqueda si se proporciona una consulta
   if (query) {
     filteredItems = filteredItems.filter((item) =>
       item.title.toLowerCase().includes(query.toLowerCase())
     );
   }
 
-  // Aplicar paginación
   const startIndex = (page - 1) * perPage;
   const endIndex = startIndex + perPage;
   const paginatedItems = filteredItems.slice(startIndex, endIndex);
@@ -57,7 +48,6 @@ app.get("/api/items", (req, res) => {
   });
 });
 
-// Ruta para agregar un nuevo item
 app.post("/api/items", async (req, res) => {
   const newItem = req.body;
   db.data.items.push(newItem);
@@ -65,7 +55,6 @@ app.post("/api/items", async (req, res) => {
   res.status(201).json(newItem);
 });
 
-// Inicia el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
